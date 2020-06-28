5c54132dca21f38ae605c7e1242051585933021b
NeeIt <VladViklund@gmail.com>
2020-06-28 14:24:00 +0300

    end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularDownloadDemo';
+export class AppComponent implements OnInit {
+  fileUrl;
+  //В Ангуляре есть встроеная библиотека для работы с файлами DomSanitizer
+  constructor(private sanitizer: DomSanitizer) { }
+  ngOnInit() {
+    //Данные в файле. По сути сюда пихать то, что придет с бэка
+    const data = `
+    using System;
+    public class Person
+    {
+      public Person()
+      {
+          Name = "unknown";
+      }
+      public Person(string name)
+      {
+          Name = name;
+      }
+      public string Name { get; }
+      public override string ToString()
+      {
+          return Name;
+      }
+    }
+    `;
+    //Создается файл с данными, пришедшими с бэка
+    const blob = new Blob([data], { type: 'application/octet-stream' });
+    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+  }
+
 }
